e0b4c2880ac97edf825d9911ec9c4fe1333ae24b
phuongdq <dqphuong2k2@gmail.com>
2025-07-27 11:07:42 +0700

    refactor: rename project

--- ios/NativeCamera/AppDelegate.swift
+++ ios/NativeCamera/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "LinkingApp",
+      withModuleName: "NativeCamera",
       in: window,
       launchOptions: launchOptions
     )
